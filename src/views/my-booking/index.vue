797a99dc678004a5f0462faff8dc9938873a0406
xingshuang <xingshuang@css.com.cn>
2022-05-10 18:11:05 +0800

    10.8

--- src/views/my-booking/index.vue
+++ src/views/my-booking/index.vue
@@ -35,8 +35,8 @@ export default {
   data() {
     return {
       reservation: {
-        stationName: '',
-        day: ''
+        stationName: null,
+        day: null
       },
       reservationList: []
 
