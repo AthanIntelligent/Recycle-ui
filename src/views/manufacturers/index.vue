12e1e7fb3e1ca1a83dde440f91baffa6ccbda97e
aiden <1794335641@qq.com>
2022-05-19 16:46:31 +0800

    厂商添加

--- src/views/manufacturers/index.vue
+++ src/views/manufacturers/index.vue
@@ -13,86 +13,48 @@
       </el-form-item>
     </el-form>
     <el-button @click="isExistGoods"  :loading="goodsList.length>0?false:true">添加</el-button>
-<!--    <el-table -->
-<!--              v-loading="loading"-->
-<!--              element-loading-text="玩命加载中"-->
-<!--              element-loading-spinner="el-icon-loading"-->
-<!--              :data="goodsList.slice((currentPage-1)*pagesize,currentPage*pagesize)" highlight-current-row style="width: 100%;font-size: 16px">-->
-<!--      <el-table-column type="index" width="100" label="序号">-->
-<!--      </el-table-column>-->
-<!--      <el-table-column-->
-<!--        prop="goodsName"-->
-<!--        label="物品名称"-->
-<!--        width="130">-->
-<!--      </el-table-column>-->
-<!--      <el-table-column-->
-<!--        prop="goodsType"-->
-<!--        label="物品类型"-->
-<!--        width="130">-->
-<!--      </el-table-column>-->
-<!--      <el-table-column-->
-<!--        prop="recycleDetail"-->
-<!--        label="简介">-->
-<!--      </el-table-column>-->
-<!--      <el-table-column-->
-<!--        prop="perMoney"-->
-<!--        label="单价">-->
-<!--        <template slot-scope="scope">-->
-<!--          {{scope.row.perMoney+''+scope.row.unit}}-->
-<!--        </template>-->
-<!--      </el-table-column>-->
-<!--      <el-table-column-->
-<!--        prop="pic"-->
-<!--        label="物品图片">-->
-<!--        <template slot-scope="scope">-->
-<!--          <img-->
-<!--            class="el-upload-list__item-thumbnail"-->
-<!--            :src='path+scope.row.pic' alt="" width="50%"-->
-<!--          >-->
-<!--        </template>-->
-<!--      </el-table-column>-->
-<!--      <el-table-column-->
-<!--        prop="remark"-->
-<!--        label="备注">-->
-<!--      </el-table-column>-->
-<!--      <el-table-column-->
-<!--        prop="createUser"-->
-<!--        label="创建人">-->
-<!--      </el-table-column>-->
-<!--      <el-table-column-->
-<!--        prop="createTime"-->
-<!--        label="创建时间">-->
-<!--      </el-table-column>-->
-<!--      <el-table-column-->
-<!--        fixed="right"-->
-<!--        label="操作"-->
-<!--        width="120">-->
-<!--        <template slot-scope="scope">-->
-<!--          <el-button-->
-<!--            @click="dialogVisible = true, getGoods(scope.row.uuid)"-->
-<!--            type="text"-->
-<!--            size="middle">-->
-<!--            修改-->
-<!--          </el-button>-->
-<!--          <el-button-->
-<!--            @click="delGoods(scope.row.uuid)"-->
-<!--            type="text"-->
-<!--            size="middle">-->
-<!--            删除-->
-<!--          </el-button>-->
-<!--        </template>-->
-<!--      </el-table-column>-->
-<!--    </el-table>-->
-<!--    <el-pagination-->
-<!--      style="position: absolute;right:660px;bottom:15px;"-->
-<!--      @size-change="handleSizeChange"-->
-<!--      @current-change="handleCurrentChange"-->
-<!--      :current-page="currentPage"-->
-<!--      :page-sizes="[5, 10, 15]"-->
-<!--      :page-size="pagesize"-->
-<!--      layout="total, sizes, prev, pager, next, jumper"-->
-<!--      :total="goodsList.length">-->
-<!--    </el-pagination>-->
+    <el-table
+      v-loading="loading2"
+      element-loading-text="玩命加载中"
+      element-loading-spinner="el-icon-loading"
+      :data="manufactures"
+      highlight-current-row
+      style="width: 100%;font-size: 16px">
+      <el-table-column type="index" width="80" label="序号" align="center">
+      </el-table-column>
+      <el-table-column
+        prop="factureName"
+        label="厂商名称"
+        align="center">
+      </el-table-column>
+      <el-table-column
+        prop="factureAddress"
+        label="厂址"
+        align="center">
+      </el-table-column>
+      <el-table-column
+        prop="facturePhone"
+        label="联系电话"
+        align="center">
+      </el-table-column>
+      <el-table-column
+        prop="createTime"
+        label="创建时间"
+        align="center">
+      </el-table-column>
+    </el-table>
+
+
+    <el-pagination
+      style="position: absolute;right:660px;bottom:15px;"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage"
+      :page-sizes="[5, 10, 15]"
+      :page-size="pagesize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="manufactures.length">
+    </el-pagination>
     <el-dialog
       title="添加厂商"
       :visible.sync="dialogVisible"
@@ -120,6 +82,9 @@ export default {
   data() {
     return {
       dialogVisible:false,
+      currentPage: 1,
+      pagesize: 10,
+      loading2: true,
       loading: true,
       manufacture:{
         factureName:'',
@@ -163,6 +128,12 @@ export default {
         this.accountTip('warning','提示','还未获取到物品列表，请耐心等待');
         this.dialogVisible = false
       }
+    },
+    handleSizeChange: function (size) {
+      this.pagesize = size
+    },
+    handleCurrentChange: function (currentPage) {
+      this.currentPage = currentPage
     },
     accountTip(type,title,info) {
       this.$notify({
